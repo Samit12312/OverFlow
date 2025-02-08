6deb3c99e7c92ae862c0372902faa9ebb97e99f8
amjokers <amjokers@DESKTOP-0IFLB19>
2025-02-08 18:26:11 +0200

    Add project files.

---
+++ IfItsBlackShoot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfItsBlackShoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+;
+        }
+        static void Targil1()// 1. num 2.if num == 0 3.nah 4.i dont know 
+        {
+            Console.WriteLine("insert number");
+            int num = int.Parse(Console.ReadLine());
+            Printevennumber(num);
+        }
+        static void Printevennumber(int num)
+        {
+            if (num == 0) { return; }
+            int digit = num % 10;
+            Printevennumber(digit);
+            if (digit % 2 == 0)
+                Console.WriteLine(digit );
+        }
+
+    }
+}
