d4cd188cfe46e7a384a275df8808f1b621829347
Owner <Owner@660084-COMPN-03>
2025-02-09 13:29:17 +0200

    jgvngchgfd

--- IfItsBlackShoot/Program.cs
+++ IfItsBlackShoot/Program.cs
@@ -11,7 +11,37 @@ namespace IfItsBlackShoot
     {
         static void Main(string[] args)
         {
-;
+            
+            Console.WriteLine(Targil5(869419));
+            Console.Read();
+        }
+        static int Targil6(int[] a, int l)
+        {
+            if (l == -1) return 0;
+            if (a[l] > 0) return 1 + Targil6(a, l - 1);
+            return Targil6(a,l - 1);
+        }
+        static int Targil5(int num)
+        {
+            if (num > -1 && num < 10) return num;
+            return Targil5(num % 10 + Targil5(num / 10));
+        }
+        static int Targil4(int n)
+        {
+            if (n == 0) return 0;
+            return n * n + Targil4(n - 1);
+        }
+        static int Targil3(int x, int y)
+        {
+            if (y == 0) return 1;
+            return x * Targil3(x, y - 1);
+        }
+        static int Targil2(int x, int y) 
+        {
+  
+            if (x > y)
+            return 0;
+            return 1 + Targil2(x + x, y);
         }
         static void Targil1()// 1. num 2.if num == 0 3.nah 4.i dont know 
         {
@@ -23,9 +53,10 @@ namespace IfItsBlackShoot
         {
             if (num == 0) { return; }
             int digit = num % 10;
-            Printevennumber(digit);
             if (digit % 2 == 0)
-                Console.WriteLine(digit );
+                Console.WriteLine(digit);
+            Printevennumber(num / 10);
+
         }
 
     }
